daba57d7229420ee4532b444eb999b3b5edf4e02
caiqiang <caiqiang@kuaijiankang.com>
2017-12-12 17:39:44 +0800

    Initial Commit

---
+++ TwoSidedView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoSidedView
+//
+//  Created by 蔡强 on 2017/12/12.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoSidedView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwoSidedView
+//
+//  Created by 蔡强 on 2017/12/12.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
